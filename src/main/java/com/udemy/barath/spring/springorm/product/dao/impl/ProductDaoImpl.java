53c10db8a3665e7a17acacd506ddc653ee6d2247
Audra Wathen <mrsbluerose@gmail.com>
2021-04-28 16:44:15 -0600

    continues spring orm unit

--- src/main/java/com/udemy/barath/spring/springorm/product/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/udemy/barath/spring/springorm/product/dao/impl/ProductDaoImpl.java
@@ -1,12 +1,16 @@
 package com.udemy.barath.spring.springorm.product.dao.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
 
 import com.udemy.barath.spring.springorm.product.dao.ProductDAO;
 import com.udemy.barath.spring.springorm.product.model.Product;
 
+@Component
 public class ProductDaoImpl implements ProductDAO {
 	
+	@Autowired
 	HibernateTemplate hibernateTemplate;
 
 	@Override
